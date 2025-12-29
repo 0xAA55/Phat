57db64f50c3db8e25dc126bbb98abccf2d038cf2
0xAA55 <weixiaobao213@qq.com>
2025-12-29 14:11:30 +0800

    编写最小测试代码。

--- Phat/main.c
+++ Phat/main.c
@@ -1,7 +1,24 @@
-﻿
+
 #include <stdio.h>
+
+#include "phat.h"
+
+Phat_t Phat;
+
+void Error_Handler()
+{
+	for (;;);
+}
+
+#define V(x) if (x != PhatState_OK) Error_Handler()
 
 int main()
 {
-    
+	V(Phat_Init(&Phat));
+
+	V(Phat_Mount(&Phat, 0));
+
+
+	V(Phat_DeInit(&Phat));
+	return 0;
 }

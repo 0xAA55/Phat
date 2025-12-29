d43ce32a1e26e880513d48e22e889cbbb63dd0dc
0xAA55 <weixiaobao213@qq.com>
2025-12-29 20:12:11 +0800

    编写测试函数，测试能不能打开目录。

--- Phat/main.c
+++ Phat/main.c
@@ -3,7 +3,7 @@
 
 #include "phat.h"
 
-Phat_t Phat;
+Phat_t phat;
 
 void Error_Handler()
 {
@@ -14,11 +14,24 @@ void Error_Handler()
 
 int main()
 {
-	V(Phat_Init(&Phat));
+	Phat_DirInfo_t dir_info;
+	WChar_t path[256] = L"";
+	PhatState res = PhatState_OK;
+	V(Phat_Init(&phat));
 
-	V(Phat_Mount(&Phat, 0));
+	V(Phat_Mount(&phat, 0));
 
+	V(Phat_OpenDir(&phat, path, &dir_info));
+	while (res == PhatState_OK)
+	{
+		res = Phat_NextDirItem(&phat, &dir_info);
+		if (dir_info.attributes & ATTRIB_DIRECTORY)
+			printf("Dir:  %S\n", dir_info.LFN_name);
+		else
+			printf("File: %S\n", dir_info.LFN_name);
+	}
+	V(Phat_CloseDir(&phat, &dir_info));
 
-	V(Phat_DeInit(&Phat));
+	V(Phat_DeInit(&phat));
 	return 0;
 }

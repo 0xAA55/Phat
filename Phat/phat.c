73de0df86fe18b1bf5e65dd2dc9443bf0a75c716
0xAA55 <weixiaobao213@qq.com>
2025-12-31 17:42:34 +0800

    将 `Phat_ReadFAT()` 和 `Phat_WriteFAT` 弄到一起。

--- Phat/phat.c
+++ Phat/phat.c
@@ -106,6 +106,7 @@ typedef struct Phat_LFN_Entry_s
 #define CI_BASENAME_IS_LOWER 0x10
 
 static PhatState Phat_ReadFAT(Phat_p phat, uint32_t cluster_index, uint32_t *read_out);
+static PhatState Phat_WriteFAT(Phat_p phat, uint32_t cluster_index, uint32_t write);
 
 static WChar_t Cp437_UpperPart[] =
 {

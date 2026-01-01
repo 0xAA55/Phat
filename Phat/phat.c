12fcbf3ba9dfa6a3106ad43d94dcc55fca800108
0xAA55 <weixiaobao213@qq.com>
2026-01-01 16:44:25 +0800

    实现 `Phat_MovePathToEditablePlace()` 将路径字符串转移到可编辑的内存区域里。

--- Phat/phat.c
+++ Phat/phat.c
@@ -200,6 +200,12 @@ static int Phat_Wcscmp(WChar_p s1, const WChar_p s2)
 	return memcmp(s1, s2, len1 * sizeof(WChar_t));
 }
 
+static void Phat_MovePathToEditablePlace(Phat_p phat, const WChar_p *path)
+{
+	if (*path < phat->path_buffer || *path >= &phat->path_buffer[MAX_PATH])
+		*(WChar_p *)path = Phat_Wcsncpy(phat->path_buffer, *path, MAX_PATH);
+}
+
 static void Phat_MoveCachedSectorHead(Phat_p phat, Phat_SectorCache_p sector)
 {
 	Phat_SectorCache_p prev = sector->prev;
